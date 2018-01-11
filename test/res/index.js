f14220f0b402610ddf79ee9b5fdc311ab57becd8
Panit Tuangsuwan <panit.tuangsuwan@thomsonreuters.com>
2018-01-11 14:31:28 +0700

    CHANGE: file licensing

---
+++ test/res/index.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2018 - present, Architecode Corporation. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT License.
+ * The LICENSE file can be found in the root directory.
+ * 
+ * @cix-lic
+ */
+
+123
+456
+789
